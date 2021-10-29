68d113f92081ee00388c5140c2fd88bc50515e0a
Vignesh16675 <93401721+Vignesh16675@users.noreply.github.com>
2021-10-29 23:41:30 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -136,7 +136,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp...')}`);
         });
 
         console.log(
-            chalk.green.bold('✅ Nandhutty working!')
+            chalk.green.bold('✅ puliyur_kotta v1 working!')
         );
         await new Promise(r => setTimeout(r, 1100));
 
